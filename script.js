f52f2d7fd47aab8a84f72aad3dd2478337a7c7df
KramYengir <kramyengir@gmail.com>
2023-06-19 21:29:43 +0100

    add library add & remove functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const sideCollar = document.querySelector('.side-collar');
 const side = document.querySelector('.side');
 
+let library = [];
+
 let Book = function(title, author, pages, genre, summary, read){
     this.title = title,
     this.author = author,
@@ -13,6 +15,18 @@ let Book = function(title, author, pages, genre, summary, read){
 let newBook = new Book('The Hunters', 'Jim', 256, 
                         'Horror', 'Really scary', false);
 
+
+function addBookToLibrary(book){
+    library.push(book);
+}
+
+function removeBookFromlibrary(book){
+    let index = library.findIndex((item) =>{
+        item === book;
+    });
+
+    library.splice(index, 1);
+}
 
 sideCollar.addEventListener('click', ()=>{
     side.classList.toggle('active');
