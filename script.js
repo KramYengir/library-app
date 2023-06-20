66069e95e49e818be6e4a1aeceed8156955feed8
KramYengir <kramyengir@gmail.com>
2023-06-20 14:59:09 +0100

    add book to library & clear form on submit

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const sideCollar = document.querySelector('.side-collar');
 const side = document.querySelector('.side');
+const addBookButton = document.querySelector('#add-book-button')
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const genreInput = document.querySelector('#genre');
+const summaryInput = document.querySelector('#summary');
 
 let library = [];
 
@@ -12,21 +19,54 @@ let Book = function(title, author, pages, genre, summary, read){
     this.read = read
 }
 
-let newBook = new Book('The Hunters', 'Jim', 256, 
-                        'Horror', 'Really scary', false);
+let newBook = new Book('The Hunters', 'Jim', 256, 'Horror', 'Really scary', false);    
 
+
+
+function createBookFromForm(){
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let genre = genreInput.value;
+    let summary = summaryInput.value;
+    
+    let newBook = new Book(title, author, pages, genre, summary, false);
+
+    addBookToLibrary(newBook);
+}
 
 function addBookToLibrary(book){
     library.push(book);
 }
 
-function removeBookFromlibrary(book){
-    let index = library.findIndex((item) =>{
+function removeBookFromlibrary(index){
+    /*  let index = library.findIndex((item) =>{
         item === book;
-    });
-
+    }); */
+    
     library.splice(index, 1);
 }
+
+function clearForm(){
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    genreInput.value = '';
+    summaryInput.value = '';
+}
+
+//This prevents the form from submitting
+//but keeps form validation, so we'll use this
+//instead of a click event on the button
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    createBookFromForm();
+
+    //Clear form and collapse side panel
+    clearForm();
+})
+
 
 sideCollar.addEventListener('click', ()=>{
     side.classList.toggle('active');
